5f24ddb864bca5c8f1c830de280255244a763214
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-05-12 13:56:39 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,4 +1,4 @@
 class Config(object):
-    API_ID = ""
-    API_HASH = ""
-    BOT_TOKEN = ""
+    API_ID = "2497276"
+    API_HASH = "78122b16901bda6c500eebe931b31d42"
+    BOT_TOKEN = "1895398933:AAELt7CjzudsJfzIP8JrvO5mGEMSgtBRuPU"
